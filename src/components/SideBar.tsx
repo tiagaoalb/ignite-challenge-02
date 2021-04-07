72a287c2b0fa446dae05a52310934f1b5b154f85
Tiago <tiagaoalb@gmail.com>
2021-04-07 14:47:21 -0300

    :tada: Challenge Finalized

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
+import { Button } from '../components/Button';
+import { useMovies } from '../hooks/useMovies';
+
 export function SideBar() {
-  // Complete aqui
+  const { setSelectedGenreId, genres, selectedGenreId } = useMovies();
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
